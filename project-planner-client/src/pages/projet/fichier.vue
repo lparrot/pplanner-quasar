075757051b7403dc847c06cda2116ee4d62166ab
kesto <laurent.parrot78@gmail.com>
2019-05-30 13:15:31 +0200

    Pouvoir afficher les fichiers du projet et les télécharger

--- project-planner-client/src/pages/projet/fichier.vue
+++ project-planner-client/src/pages/projet/fichier.vue
@@ -1,10 +1,40 @@
 <template>
   <q-page padding>
-    <q-uploader ref="uploader" batch multiple @uploaded="uploaded" :url="'/api/projets/fichiers/' + selectedProjet.id" class="full-width" />
 
-    <q-separator class="q-my-sm" />
+    <q-table title="Fichiers" flat selection="multiple" :selected.sync="selected" :pagination="pagination" dense :data="selectedProjet.fichiers" :columns="columns" row-key="nom">
+      <template v-slot:top>
+        <template v-if="selected.length">
+          <q-btn flat dense icon="delete" color="primary" :label="getLabel('Supprimer')" @click="deleteFiles">
+            <q-tooltip>Supprimer</q-tooltip>
+          </q-btn>
+          <q-btn flat dense icon="unarchive" color="primary" :label="getLabel('Télécharger')" @click="downloadFiles">
+            <q-tooltip>Télécharger</q-tooltip>
+          </q-btn>
+          <q-separator class="q-mx-md" vertical />
+        </template>
+        <q-btn flat dense icon="archive" color="primary" :label="getLabel('Upload')" @click="$refs.modalUpload.show()">
+          <q-tooltip>Upload</q-tooltip>
+        </q-btn>
+      </template>
+    </q-table>
 
-    <q-table title="Fichiers" :pagination="pagination" dense :data="selectedProjet.fichiers" :columns="columns" row-key="nom" />
+    <q-dialog ref="modalUpload">
+      <q-layout container class="bg-white" style="max-height: 50vh">
+        <q-header class="bg-primary">
+          <q-toolbar>
+            <q-toolbar-title>Chargement de fichiers</q-toolbar-title>
+            <q-btn flat v-close-popup round dense icon="close" />
+          </q-toolbar>
+        </q-header>
+
+        <q-page-container>
+          <q-page padding>
+            <q-uploader ref="uploader" flat batch multiple no-thumbnails @uploaded="uploaded" :url="'/api/projets/fichiers/' + selectedProjet.id" class="full-width" />
+          </q-page>
+        </q-page-container>
+
+      </q-layout>
+    </q-dialog>
   </q-page>
 </template>
 
@@ -15,19 +45,47 @@ export default {
   name: 'page-fichier',
   mixins: [projetMixin],
   data: () => ({
+    rightDrawer: false,
+    selected: [],
     fichier: null,
     pagination: {
       rowsPerPage: 15,
+      sortBy: 'nom',
     },
     columns: [
-      { name: 'nom', field: 'nom', align: 'left' },
-      { name: 'type', field: 'type' },
+      { label: 'Fichier', name: 'nom', field: 'nom', align: 'left', sortable: true },
+      { label: 'Type de fichier', name: 'type', field: 'type', sortable: true },
     ],
   }),
   methods: {
+    getLabel(label) {
+      return this.$q.screen.gt.sm ? label : null
+    },
     uploaded(info) {
       this.selectedProjet = JSON.parse(info.xhr.response)
       this.$refs.uploader.reset()
+    },
+    async deleteFiles() {
+
+    },
+    async downloadFiles() {
+      const res = await this.$axios({
+        url: `/api/projets/fichiers/download`,
+        method: 'POST',
+        responseType: 'blob',
+        data: this.selected.map(data => {
+          return { id: data.id }
+        }),
+      })
+
+      console.log(res)
+
+      const url = window.URL.createObjectURL(new Blob([res.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', res.headers['filename']); //or any other extension
+      document.body.appendChild(link);
+      link.click();
     },
   },
 }
--- project-planner-server/src/main/java/fr/lauparr/project_planner/server/controller/ProjetFichierController.java
+++ project-planner-server/src/main/java/fr/lauparr/project_planner/server/controller/ProjetFichierController.java
@@ -6,16 +6,29 @@ import fr.lauparr.project_planner.server.projections.ProjetDTO;
 import fr.lauparr.project_planner.server.repository.ProjetFichierRepository;
 import fr.lauparr.project_planner.server.repository.ProjetRepository;
 import fr.lauparr.project_planner.server.service.ProjectionService;
+import lombok.Data;
+import org.apache.commons.compress.archivers.ArchiveException;
+import org.apache.commons.compress.archivers.ArchiveOutputStream;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/projets/fichiers")
@@ -36,7 +49,7 @@ public class ProjetFichierController {
     }
     List<String> nomFichiers = projetFichierRepository.findAllNomFichierByProjet(id);
     Iterator<String> filenames = request.getFileNames();
-    while(filenames.hasNext()) {
+    while (filenames.hasNext()) {
       MultipartFile file = request.getFile(filenames.next());
       String fileName = StringUtils.cleanPath(file.getOriginalFilename());
       if (nomFichiers.stream().noneMatch(fileName::equals)) {
@@ -50,5 +63,53 @@ public class ProjetFichierController {
     }
     return ResponseEntity.ok(projectionService.convertToDto(projet, ProjetDTO.class));
   }
+
+  @PostMapping(value = "/download", produces = "application/octet-stream")
+  public ResponseEntity download(@RequestBody List<PostProjetFichierDownloadParams> params) throws IOException, ArchiveException {
+
+    byte[] binary = new byte[0];
+    String filename = "";
+
+    if (params.size() == 1) {
+      ProjetFichier fichier = projetFichierRepository.getOne(params.get(0).getId());
+      filename = fichier.getNom();
+      binary = fichier.getData();
+    } else {
+      filename = "archive.zip";
+      ByteArrayOutputStream archiveStream = new ByteArrayOutputStream();
+      ArchiveOutputStream archive = new ArchiveStreamFactory().createArchiveOutputStream(ArchiveStreamFactory.ZIP, archiveStream);
+
+
+      for (PostProjetFichierDownloadParams param : params) {
+        List<ProjetFichier> fichiers = projetFichierRepository.findAllById(params.stream().map(PostProjetFichierDownloadParams::getId).collect(Collectors.toList()));
+        for (ProjetFichier fichier : fichiers) {
+          ZipArchiveEntry entry = new ZipArchiveEntry(fichier.getNom());
+          archive.putArchiveEntry(entry);
+
+          BufferedInputStream input = new BufferedInputStream(new ByteArrayInputStream(fichier.getData()));
+
+          IOUtils.copy(input, archive);
+          input.close();
+          archive.closeArchiveEntry();
+        }
+      }
+
+      archive.finish();
+      binary = archiveStream.toByteArray();
+      archiveStream.close();
+    }
+
+    return ResponseEntity.ok()
+      .header("filename", filename)
+      .contentType(MediaType.APPLICATION_OCTET_STREAM)
+      .contentLength(binary.length)
+      .body(binary);
+  }
+
+  @Data
+  private static class PostProjetFichierDownloadParams {
+    private Long id;
+  }
+
 
 }
